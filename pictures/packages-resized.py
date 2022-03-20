bf5c16d08738786017666dc0b0219eacc853801f
Peter Kováč <47066564+kovy21@users.noreply.github.com>
2022-03-20 23:10:58 +0100

    Add files via upload

---
+++ pictures/packages-resized.py
@@ -0,0 +1,11 @@
+# Packages necessary:
+#import sys
+#pip install tensorflow
+#pip install FER
+
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tensorflow']) # tensorflow for keras models 
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'FER']) # facial recognition
